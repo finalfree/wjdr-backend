3c9774a8326275021976105b53fa3c5351bf8946
cogear <cogear@qq.com>
2025-01-18 14:26:56 +0800

    init go project

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(20)"`
+	Level     int       `json:"level"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func main() {
+	r := gin.Default()
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	r.GET("/users", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(200, users)
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+		db.Create(&user)
+		c.JSON(201, user)
+	})
+
+	r.Run()
+}
